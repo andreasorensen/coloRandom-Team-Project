f4ec7daaf50bf3b234c8be08be336451dd8b94d3
Parvin Sattorova <nanajon66@gmail.com>
2023-04-11 00:44:28 -0600

    Add src files, html layout, css initial layout

--- main.js
+++ main.js
@@ -13,15 +13,6 @@ window.addEventListener('load', displayRandomPalette)
 
 var currentPalette;
 
-function createHexCode() {
-  var hexCharacters = ["A", "B", "C", "D", "E", "F", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-  var hexCode = []
-  for (var i = 0; i < 6; i++) {
-    hexCode.push(hexCharacters[getRandomIndex(hexCharacters)])
-  }
-  return `#${hexCode.join('')}`
-}
-
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length)
 }
